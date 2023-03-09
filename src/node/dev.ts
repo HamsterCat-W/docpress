b091ae454abcb9fbdf762a5040684c72cb94231c
guoxuan.wang <guoxuan.wang@transsion.com>
2023-03-09 17:30:49 +0800

    chore:热更新接入

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,9 +1,10 @@
 import { createServer as createViteDevServer } from "vite";
 import { indexHtmlPlugin } from "./plugin/indexHtml";
+import pluginInReact from "@vitejs/plugin-react";
 
 export async function createDevServer(root: string = process.cwd()) {
   return createViteDevServer({
     root,
-    plugins: [indexHtmlPlugin()],
+    plugins: [indexHtmlPlugin(), pluginInReact()],
   });
 }
--- src/node/plugin/indexHtml.ts
+++ src/node/plugin/indexHtml.ts
@@ -11,6 +11,11 @@ export function indexHtmlPlugin(): Plugin {
         server.middlewares.use(async (req, res, next) => {
           let html = await readFile(defaultIndexPath, "utf-8");
           try {
+            html = await server.transformIndexHtml(
+              req.url,
+              html,
+              req.originalUrl
+            );
             res.statusCode = 200;
             res.setHeader("Content-Type", "text/html");
             res.end(html);
