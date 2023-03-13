ec94660bf36685564f69f778e3dc588a572a33d8
guoxuan.wang <guoxuan.wang@transsion.com>
2023-03-13 19:17:24 +0800

    chore:config update

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -2,9 +2,14 @@ import { createServer as createViteDevServer } from "vite";
 import { indexHtmlPlugin } from "./plugin/indexHtml";
 import pluginInReact from "@vitejs/plugin-react";
 
-export async function createDevServer(root: string = process.cwd()) {
+export function createDevServer(root: string = process.cwd()) {
   return createViteDevServer({
     root,
+    server: {
+      port: 7779,
+      host: true,
+      open: "/",
+    },
     plugins: [indexHtmlPlugin(), pluginInReact()],
   });
 }
