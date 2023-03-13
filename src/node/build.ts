fabc6198f366620cd1aed8fe2b14a622fc50427c
guoxuan.wang <guoxuan.wang@transsion.com>
2023-03-13 20:08:50 +0800

    chore:性能优化，公用代码提取和阻塞式编码修改，采用promise.all实现并行操作

--- src/node/build.ts
+++ src/node/build.ts
@@ -2,49 +2,45 @@ import { build as viteBuild } from "vite";
 import { clientEntryPath, serverEntryPath } from "./constant";
 
 async function bundle(root: string) {
-  // 客户端构建
-  const clientViteBuild = () =>
-    viteBuild({
-      mode: "production",
-      root,
-      build: {
-        outDir: "build",
-        rollupOptions: {
-          // 入口
-          input: clientEntryPath,
-          //   出口
-          output: {
-            format: "esm",
+  try {
+    // 构建函数
+    const buildByFlag = (isServer: boolean) => {
+      return viteBuild({
+        mode: "production",
+        root,
+        build: {
+          ssr: isServer,
+          outDir: isServer ? ".temp" : "build",
+          rollupOptions: {
+            input: isServer ? serverEntryPath : clientEntryPath,
+            output: {
+              format: isServer ? "cjs" : "esm",
+            },
           },
         },
-      },
-    });
-  // 服务端构建
-  const serverViteBuild = () =>
-    viteBuild({
-      mode: "production",
-      root,
-      build: {
-        ssr: true,
-        outDir: ".temp",
-        rollupOptions: {
-          input: serverEntryPath,
-          output: {
-            format: "cjs",
-          },
-        },
-      },
-    });
+      });
+    };
+    // 客户端构建
+    const clientViteBuild = () => buildByFlag(false);
+    // 服务端构建
+    const serverViteBuild = () => buildByFlag(true);
 
-  console.log("🚀 ~ file: build.ts:41 ~ bundle ~ : client and server");
-  await clientViteBuild();
-  await serverViteBuild();
+    console.log("🚀 ~ file: build.ts:41 ~ bundle ~ : client and server");
+    const [clientBundle, serverBundle] = await Promise.all([
+      clientViteBuild(),
+      serverViteBuild(),
+    ]);
+
+    return [clientBundle, serverBundle];
+  } catch (error: any) {
+    console.log("🚀 ~ file: build.ts:34 ~ bundle ~ error:", error);
+  }
 }
 
 // 构建函数
 export async function build(root: string = process.cwd()) {
   // 1、bundle--client 和 server 端
-  await bundle(root);
+  const [clientBundle, serverBundle] = await bundle(root);
   // 2、引入 server-entry
   // 3、服务端渲染，产出 html
 }
