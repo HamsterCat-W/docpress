d86ed75385cf041c89e6c0036d729aa39027227f
guoxuan.wang <guoxuan.wang@transsion.com>
2023-03-14 15:13:19 +0800

    chore:注释增加

--- tsup.config.ts
+++ tsup.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   dts: true,
   shims: true,
   clean: true,
-  // // 使用它可以在生成的 JavaScript 和 CSS 文件的开头插入任意字符串。这样就解决了
+  // // 使用它可以在生成的 JavaScript 和 CSS 文件的开头插入任意字符串。这样就解决了throw new Error('Dynamic require of "' + x + '" is not supported');报错问题
   banner: {
     js: 'import { createRequire as createRequire0 } from "module"; const require = createRequire0(import.meta.url);',
   },
