7d0969f9e80a4325902c3b144030bf6e41c3687e
guoxuan.wang <guoxuan.wang@transsion.com>
2023-03-14 15:17:40 +0800

    docs:库工具包构建方案

---
+++ src/test/add.cjs
@@ -0,0 +1,14 @@
+// 正常情况下
+// const { add } = require("./add.mjs");
+
+// let res = add(1, 2);
+// console.log("🚀 ~ file: add.cjs:4 ~ res:", res);
+
+// 异步环境下
+async function foo() {
+  const { add } = await import("./util.mjs");
+  let res = add(1, 2);
+  console.log("🚀 ~ file: add.cjs:11 ~ foo ~ res:", res);
+}
+
+foo();
---
+++ src/test/add.mjs
@@ -0,0 +1,4 @@
+import { add } from "./util.cjs";
+
+let res = add(1, 2);
+console.log("🚀 ~ file: add.mjs:4 ~ res:", res);
