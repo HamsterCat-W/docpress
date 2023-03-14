00fb083a29d48370068de5b6447e0ebd50504c92
guoxuan.wang <guoxuan.wang@transsion.com>
2023-03-14 14:20:09 +0800

    chore:tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/node/cli.ts"],
+  // 开启bundle模式
   bundle: true,
   outDir: "dist",
   splitting: true,
@@ -9,4 +10,8 @@ export default defineConfig({
   dts: true,
   shims: true,
   clean: true,
+  // // 使用它可以在生成的 JavaScript 和 CSS 文件的开头插入任意字符串。这样就解决了
+  banner: {
+    js: 'import { createRequire as createRequire0 } from "module"; const require = createRequire0(import.meta.url);',
+  },
 });
